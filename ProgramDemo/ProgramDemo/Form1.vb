7e9781cc9a365ffe2f436cd1e23f16b69952f68f
alissabueno <94299513+alissabueno@users.noreply.github.com>
2023-05-25 00:08:42 +0800

    Add files via upload

---
+++ ProgramDemo/ProgramDemo/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.Common
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    Dim connectionString As String = "Server=127.0.0.1;Database=trialdb;Uid=root;Pwd=alissamoresbueno;"
+    Dim connection As MySqlConnection = New MySqlConnection(connectionString)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Module1.BackupTableToExcel("MyTable", "C:\Users\User\OneDrive\Documents\template.xlsx", "C:\Users\User\Desktop\MyBackup.xlsx")
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim query As String = "SELECT * FROM users"
+        Dim adapter As MySqlDataAdapter = New MySqlDataAdapter(query, connection)
+        Dim table As DataTable = New DataTable()
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+    End Sub
+End Class
---
+++ ProgramDemo/ProgramDemo/Module1.vb
@@ -0,0 +1,53 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Microsoft.Office.Interop.Excel
+Module Module1
+    Public Sub BackupTableToExcel(ByVal tableName As String, ByVal excelTemplatePath As String, ByVal excelOutputPath As String)
+
+        ' Open MySQL connection
+        Dim conn As MySqlConnection = New MySqlConnection("Server=127.0.0.1;Database=trialdb;Uid=root;Pwd=alissamoresbueno;")
+        conn.Open()
+
+        ' Create SQL command to select data from table
+        Dim cmd As MySqlCommand = New MySqlCommand(String.Format("SELECT * FROM users", tableName), conn)
+
+        ' Create MySQL data adapter
+        Dim adapter As MySqlDataAdapter = New MySqlDataAdapter(cmd)
+
+        ' Create new dataset
+        Dim dataset As DataSet = New DataSet()
+
+        ' Fill dataset with data from MySQL
+        adapter.Fill(dataset, "MyTable")
+
+        ' Close MySQL connection
+        conn.Close()
+
+        ' Open Excel template
+        Dim excelApp As Application = New Application()
+        Dim excelWorkbook As Workbook = excelApp.Workbooks.Open(excelTemplatePath)
+
+        ' Get Excel worksheet
+        Dim excelWorksheet As Worksheet = CType(excelWorkbook.Sheets("Sheet1"), Worksheet)
+
+        ' Write dataset to Excel worksheet
+        Dim rows As DataRowCollection = dataset.Tables("MyTable").Rows
+        For i As Integer = 0 To rows.Count - 1
+            Dim row As DataRow = rows(i)
+            For j As Integer = 0 To row.ItemArray.Length - 1
+                Dim value As Object = row.ItemArray(j)
+                excelWorksheet.Cells(i + 10, j + 3) = value
+            Next
+        Next
+
+        ' Save Excel workbook
+        excelWorkbook.SaveAs(excelOutputPath)
+
+        ' Close Excel workbook and application
+        excelWorkbook.Close()
+        excelApp.Quit()
+
+        ' Show a message box to indicate success
+        MessageBox.Show("Backup created successfully!")
+
+    End Sub
+End Module
